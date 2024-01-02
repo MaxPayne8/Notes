aff329f3440b2230208cbb7bcb2a33b852243910
MaxPayne8 <maxpayne0852.com>
2024-01-02 16:49:56 +0530

    commit

--- src/Components/NoteTile.js
+++ src/Components/NoteTile.js
@@ -8,12 +8,12 @@ const NoteTile = ({ title, description, date }) => {
   return (
     <div className="relative w-44 h-44 sm:w-60 sm:h-60 rounded-lg shadow-lg shadow-slate-700 border-black border-2 bg-slate-300 p-2 m-10 tranform hover:scale-110 md:hover:scale-125 transition ease-in-out group">
       <h1 className="text-center  sm:text-xl break-words ">
-        {newTitle.length < 15 ? title : `${newTitle}...`}
+        {newTitle.length < 15 ? title : `${newTitle}..`}
       </h1>
       {newDesc.length ? (
         <p
           dangerouslySetInnerHTML={{
-            __html: newDesc.length < 150 ? newDesc : `${newDesc}...`,
+            __html: newDesc.length < 150 ? newDesc : `${newDesc}..`,
           }}
           className="mt-2 break-all h-[165px] hidden md:block overflow-y-hidden "
         />
@@ -24,8 +24,7 @@ const NoteTile = ({ title, description, date }) => {
         <p
           className="mt-2 break-all h-[120px] block md:hidden overflow-y-hidden "
           dangerouslySetInnerHTML={{
-            __html:
-              newDesc.lenghth < 75 ? newDescMobile : `${newDescMobile}...`,
+            __html: newDesc.lenghth < 75 ? newDescMobile : `${newDescMobile}..`,
           }}
         />
       ) : (
