8c9f3c4e1b3ab642a5000a093a7ade26d11c3aec
MaxPayne8 <maxpayne0852.com>
2024-01-02 14:00:46 +0530

    changing-text-color-preview

--- src/Components/PreviewNote.js
+++ src/Components/PreviewNote.js
@@ -9,7 +9,7 @@ const PreviewNote = () => {
   console.log(getNotes1);
   const { title, date, description } = getNotes1[0];
   return (
-    <div className="p-2 m-2 bg-slate-900 min-h-screen text-slate-200 rounded-lg">
+    <div className="p-2 m-2 bg-slate-900 min-h-screen text-slate-300 rounded-lg">
       <h1 className="text-center text-2xl p-2 m-2 break-words"> {title}</h1>
       <h1 className="text-center p-2 m-2">({date})</h1>
       <p
